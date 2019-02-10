b4f93a69f5f9553e38462a408ddda2c45cf4865a
aniruddha <aniruddha.kudalkar@gmail.com>
2019-02-10 12:10:37 +0530

    directives : custom structural directives

--- angular/basicsapp/src/app/directives/opp-if.directive.ts
+++ angular/basicsapp/src/app/directives/opp-if.directive.ts
@@ -1,10 +1,29 @@
-import { Directive } from '@angular/core';
+import { Directive, TemplateRef, ViewContainerRef, OnInit, Input, OnChanges } from '@angular/core';
 
 @Directive({
-  selector: '[appOppIf]'
+  selector: '[oppIf]'
 })
-export class OppIfDirective {
+export class OppIfDirective implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input()
+  oppIf : boolean
 
+  constructor(
+    private tmRf: TemplateRef<any>,
+    private vcRef: ViewContainerRef
+  ) {
+    console.log(tmRf)
+    console.log(vcRef)
+  }
+
+  ngOnInit() {
+   
+  }
+  ngOnChanges() {
+    if(!this.oppIf) {
+      this.vcRef.createEmbeddedView(this.tmRf)
+    }else {
+      this.vcRef.clear()
+    }
+  }
 }
